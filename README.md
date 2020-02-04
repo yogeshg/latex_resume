0d4990126edaad2f43b93446a74f9e3fadb7ce4a
yogesh garg <1059168+yogeshg@users.noreply.github.com>
2020-02-04 00:39:16 -0800

    add markdown update

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Setup
 ```bash
     brew cask install mactex
     pip install -r requirements.txt
+    git clone https://github.com/yogeshg/markdown2latex.git
+    cd markdown2latex
+    git checkout ce9938f74ab3e28a3cf53adff794438ccbad66df
+    python setup.py install
+    cd -
 ```
+
+
 
 
--- src/latex_resume.py
+++ src/latex_resume.py
@@ -1,14 +1,28 @@
 import argh
 
+import markdown, mdx_latex
+latex_mdx = mdx_latex.LaTeXExtension()
+md = markdown.Markdown()
+latex_mdx.extendMarkdown(md, markdown.__dict__)
+
 section_format = """
-\input{default}
+\\input{default}
+\\renewcommand{\\optionA}{
+OPTIONA
+}
 """
 
 
 def get_markdown(section, output):
     with open(section, 'r') as in_file:
+        md_text = in_file.read().strip()
         with open(output, 'w') as out_file:
-            text = section_format
+            text = md.convert(md_text)
+            text = text.replace("<root>", "").replace("</root>", "")
+            if text:
+                text = section_format.replace("OPTIONA", text)
+            else:
+                text = section_format
             out_file.write(text)
 
 
