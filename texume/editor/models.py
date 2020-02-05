ac252b914de9d505fe81dc556f79fd7eebdf8311
yogesh garg <1059168+yogeshg@users.noreply.github.com>
2020-02-05 15:04:12 -0800

    add user and content models

--- texume/editor/models.py
+++ texume/editor/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+    user_name = models.CharField(max_length=200)
+
+
+class Content(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Section(models.TextChoices):
+        NAME = "Name"
+        LINK = "Link"
+        PHONE = "Phone"
+        EMAIL = "Email"
+        POSTMAIL = "Postmail"
+        EDUCATION = "Education"
+        PROFESSIONAL_EXPERIANCE = "Professional Experience"
+        PROJECT_WORK = "Project Work"
+        PUBLICATIONS = "Publications"
+        COURSES = "Courses"
+        TECHNICAL_SKILLS = "Technical Skills"
+        EXTRA_CURRICULAR = "Extra Curricular"
+    
+    section = models.CharField(max_length=128, choices=Section.choices)
+
+    body = models.TextField()
+
+    class Formatting(models.TextChoices):
+        ORG_LOC_TITLE_DATE_POINTS = "org-loc-title-date-points"
+        DATE_POINTS = "date-points"
+        TEXT = "text"
+
+    formatting = models.CharField(
+        max_length=128,
+        choices=Formatting.choices,
+        default=Formatting.TEXT
+    )
+
+    created = models.DateField(auto_now_add=True)
+
+
