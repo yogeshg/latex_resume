f101189c41c8d00fcea8744ffc49bcf206266fb6
yogesh garg <1059168+yogeshg@users.noreply.github.com>
2020-02-05 16:31:23 -0800

    maintian users in site user table; view details for a user in editor app

--- texume/editor/urls.py
+++ texume/editor/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/login/', auth_views.LoginView.as_view()),
 ]
 
--- texume/editor/views.py
+++ texume/editor/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
+from .models import Content
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the editor index.")
+
+
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the editor index.")
-
+    user_content = Content.objects.filter(user_id__exact=request.user.id)
+    return render(
+        request, "editor/form.html", {"user_content": user_content}
+    )
